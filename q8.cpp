1ad984a801bf8b1aff0001cec6cf939168f32ed0
UTKARSH SINHA <sinhau65@gmail.com>
2026-09-13 01:21:10 +0530

    feat: if else statement

--- q8.cpp
+++ q8.cpp
@@ -3,5 +3,12 @@
 #include<iostream>
 using namespace std;
 int main(){
-    
+    int age;
+    cin>>age;
+    if(age>=18){
+        cout<<"You are an adult"<<endl;
+    }
+    else{
+        cout<<"You are not an adult"<<endl;
+    }
 }
