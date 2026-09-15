7d7272067e7d333596f8ab0b67f7efc734686cae
UTKARSH SINHA <sinhau65@gmail.com>
2026-09-15 21:23:51 +0530

    feat: for loop using c++

---
+++ loop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+// printing 5 times the name entered by the user 
+//using for loop 
+int main(){
+    string name;
+    cout<<"Enter your name";
+    cin>>name;
+    for(int i=0;i<name.size();i++){
+        cout<<name<<endl;
+    }
+    return 0;
+
+}
