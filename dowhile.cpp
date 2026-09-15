c1bc51b00460d1ecd38fcc088e92187c6cc392bb
UTKARSH SINHA <sinhau65@gmail.com>
2026-09-15 21:31:10 +0530

    feat: do while loop using c++

---
+++ dowhile.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+//runs atleast a single time
+//do it for the first time and then check the condition
+int main(){
+    int i=10;
+    do{
+        cout<<i<<endl;
+        i++;
+    }
+    while(i<10);
+}
