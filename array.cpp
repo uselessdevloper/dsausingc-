217e73b152d6597326cf625c3d1d4cf85de2d883
UTKARSH SINHA <sinhau65@gmail.com>
2026-09-15 20:50:30 +0530

    feat :  1d array using c++

---
+++ array.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main(){
+    int arr[5];
+    double arr2[10];
+    cin>>arr[0];
+    cin>>arr[1];
+    cin>>arr[2];
+    cin>>arr[3];
+    cin>>arr[4];
+    arr[3]=1999;
+    cout<<arr[3]<<endl;
+    return 0;
+
+}
